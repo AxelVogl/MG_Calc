36fd6b1f8a44317cbc48f934f177c4f63e5ee950
Axel <axel.vogl@vokit.de>
2020-09-23 13:14:05 +0200

    update calc.js
    not finished yet

--- calculator/public/js/calc.js
+++ calculator/public/js/calc.js
@@ -1,35 +1,63 @@
+var bool = 0;
+var first, scnd = "", operatorClicked;
+var actScore;
+
 $(".no").on('click', function() {
     var anzeige = document.getElementById("anzeige");
     var currentScore = document.getElementById("anzeige").value;
     var numberClicked = this.value;
-    if(currentScore == "0") {
-        var actScore = parseInt(currentScore) + parseInt(numberClicked);
-    } else {
-        actScore = currentScore + numberClicked;
+
+    if(bool == false){
+        if(currentScore == "0") {
+            first = parseInt(currentScore) + parseInt(numberClicked);
+            actScore = parseInt(currentScore) + parseInt(numberClicked);
+        } else {
+            actScore = currentScore + numberClicked;
+        }
+    }else {
+            scnd = scnd + numberClicked;
+            console.log("was passiert hier? " + scnd);
+            actScore = currentScore + numberClicked;
     }
-
-
     
-
+    console.log("aaa: " + numberClicked);
     anzeige.value = actScore;
 });
 
 $(".op").on('click', function() {
     var anzeige = document.getElementById("anzeige");
     var currentScore = parseInt(anzeige.value);
-    var operatorClicked = this.value;
+    operatorClicked = this.value;
 
+    first = parseInt(currentScore);
+    bool = 1;
     anzeige.value = currentScore + operatorClicked;
-    //in input reinschreiben
 });
 
 //abfragen welcher operator drin ist und die aktion durchführen
 
 $(".ac").on('click', function() {
-    anzeige.value = 0;
+    anzeige.value = "0";
+    first = 0; 
+    scnd = ""; 
+    actScore = 0;
+    bool = 0;
+    console.log("clear "+ first + " und " + scnd + " und " + actScore);
     //in input reinschreiben
 });
 
 $(".gleich").on('click', function() {
-    //ergebnis berechnen
+    console.log("Rechnung: " + first + " " + operatorClicked + " " + scnd);
+
+
+    if(operatorClicked == "+"){
+        anzeige.value = first + parseInt(scnd);
+    } else if (operatorClicked == "-") {
+        anzeige.value = first - parseInt(scnd);
+    } else if (operatorClicked == "/"){
+        anzeige.value = first / parseInt(scnd);
+    } else if (operatorClicked == "*"){
+        anzeige.value = first * parseInt(scnd);
+    }
+ 
 });
